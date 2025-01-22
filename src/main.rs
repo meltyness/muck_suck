6cb23149bcd4d1727f2142e19f31c815198c8a6c
meltyness <86126050+meltyness@users.noreply.github.com>
2025-01-21 22:23:43 -0800

    First commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+struct msec_ent {
+    // controlled ports listing
+    //
+    // 'common port' selection
+    //
+    //
+}
+
+struct tx_req {
+
+}
+
+struct rx_noti {
+
+}
+
+
+impl msec_ent {
+    pub fn encode_pdu(data: &[u8]) -> Vec<u8> {
+
+    }
+
+    pub fn decode_pdu(data: &[u8]) -> Vec<u8> {
+
+    }
+}
