faad0cf5c8f0c2bd8f5bc9435898e16c140587cc
meltyness <86126050+meltyness@users.noreply.github.com>
2025-01-21 23:01:01 -0800

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
+// use std::crypto::*; these crypto protocol
 fn main() {
     println!("Hello, world!");
+}
+
+pub fn configure() -> msec_ent {
+
+}
+
+struct mac_pdu {
+    // dst
+    // src
+    // pri
+    // msdu
+    // FCS
+    // tag
 }
 
 struct msec_ent {
@@ -7,7 +21,36 @@ struct msec_ent {
     //
     // 'common port' selection
     //
+    // a mac address
+    // map of secure channel identifiers to port identifiers
     //
+    // kex_ent listing
+    //
+    // configuration regarding protection level used
+    //   i.e., integrity enabled/disabled
+    //   i.e., selected cipher suite (per port association?)
+    //
+    // peer listing
+    //  authenticated and authorized peers, needed details or msec_ch
+    //
+    //
+}
+
+struct msec_ch { 
+    // selected suite
+    //  associated config info max SCs,SAKs
+    //
+    // rx channel
+    //  two keys
+    // tx channel
+    //  a reference to one of the keys
+    // 
+}
+
+impl msec_ch {
+    pub fn swap_key(self: &mut Self) {
+        // swap the tx channel key
+    }
 }
 
 struct tx_req {
@@ -18,13 +61,26 @@ struct rx_noti {
 
 }
 
+struct ms_pdu {
+
+}
 
 impl msec_ent {
-    pub fn encode_pdu(data: &[u8]) -> Vec<u8> {
-
+    pub fn decode_validate_pdu(data: &[u8]) -> ms_pdu {
+        // uses some combination of validation and decryption
     }
 
-    pub fn decode_pdu(data: &[u8]) -> Vec<u8> {
+    pub fn encode_protect_pdu(data: &ms_pdu) -> Vec<u8> {
+        // uses encryption
+    }
+}
 
+struct kex_ent {
+
+}
+
+impl kex_ent {
+    pub fn layer_management(self: &mut Self, state: lm_state) {
+    
     }
 }
